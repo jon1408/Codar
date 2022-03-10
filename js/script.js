bbfed12c828c9e8932057449f9d2d5a6814da502
jon1408 <40411510+jon1408@users.noreply.github.com>
2022-03-10 00:06:40 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
